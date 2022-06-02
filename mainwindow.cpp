ff98fc236b40b090f57100325480c1b246217687
Yacine Cheikhrouhou <yc303894@ad.u-bourgogne.fr>
2022-06-02 09:34:32 +0200

    add robot stop code (not working)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -102,7 +102,7 @@ void MainWindow::on_robot_top_pressed()
     robot.DataToSend[3] = 0;
     robot.DataToSend[4] =100;
     robot.DataToSend[5] = 0;
-    robot.DataToSend[6] = 16 + 64;//32 + 16 + 128 + 64;
+    robot.DataToSend[6] = 64 + 16; // ajouter 128+32 pour asservissement
     short crc = robot.Crc16(robot.DataToSend, 7);
 
     char low = crc;
@@ -115,7 +115,17 @@ void MainWindow::on_robot_top_pressed()
 
 void MainWindow::on_robot_right_released()
 {
+    robot.DataToSend[2] = 0;
+    robot.DataToSend[3] = 0;
+    robot.DataToSend[4] = 0;
+    robot.DataToSend[5] = 0;
+    robot.DataToSend[6] = 0;
+    short crc = robot.Crc16(robot.DataToSend, 7);
 
+    char low = crc;
+    char high = crc >> 8;
+    robot.DataToSend[7] = low;
+    robot.DataToSend[8] = high;
 }
 
 void MainWindow::on_robot_buttom_released()
